b92632148a38adb7b40357a6019232fceccae988
Miranda <omirandavictor@gmail.com>
2022-03-08 06:54:58 -0300

    Configurando autenticaçao e autorizaçao de usuarios

--- routes/specifications.routes.ts
+++ routes/specifications.routes.ts
@@ -1,14 +1,12 @@
 import { Router } from 'express';
+import { ensureAuthenticated } from '../src/middlewares/ensureAuthenticated';
 import { CreateSpecificationController } from '../src/modules/cars/useCases/createSpecification/CreateSpecificationController';
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
+specificationsRoutes.use(ensureAuthenticated)
 specificationsRoutes.post("/", createSpecificationController.handle)
-
-specificationsRoutes.get("/", (request, response) => {
-
-})
 
 export { specificationsRoutes };
