c502cf418899622e30e4d8dd6f78662ff6065328
Miranda <omirandavictor@gmail.com>
2022-03-11 20:53:52 -0300

    Parte 1 - Testes Unitarios concluidos

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../../../../errors/AppError";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
@@ -15,9 +16,48 @@ describe("Authenticate User", () => {
   });
 
 
-/*   it("Shoud be able to authenticate an user", () => {
+  it("Shoud be able to authenticate an user", async () => {
     const user: ICreateUserDTO ={
+      driver_license: "000123",
+      email: "user@test.com",
+      password: "1234",
+      name: "User Test"
+    };
+    await createUserUseCase.execute(user);
 
-    }
-  }); */
+    const result = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(result).toHaveProperty("token");
+  });
+
+  it("Shouldn't be able to authenticate a nonexistent user", () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({
+        email: "false@email.com",
+        password: "1234",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Shoudn't be able to authenticate with incorrect password", () => {
+    expect( async () => {
+      const user: ICreateUserDTO = {
+        driver_license: "000123",
+        email: "user@test.com",
+        password: "1234",
+        name: "User Test"
+      };
+
+      await createUserUseCase.execute(user);
+
+      await authenticateUserUseCase.execute({
+        email: user.email,
+        password: "incorrectPassword"
+      })
+    }).rejects.toBeInstanceOf(AppError)
+  })
+
 });
